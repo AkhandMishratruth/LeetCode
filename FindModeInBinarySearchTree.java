9bf0f2392f206a167688d7335eeec1e46b7bb1fb
AkhandMishratruth <apratap.mishra.cer15@itbhu.ac.in>
2018-06-21 11:00:14 +0530

    501

---
+++ FindModeInBinarySearchTree.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    static HashMap<Integer, Integer> map;
+    public int[] findMode(TreeNode root) {
+        map = new HashMap<Integer, Integer>();a
+        fun(root);
+        int len=0, freq=0;
+        for(Map.Entry<Integer, Integer> ent : map.entrySet()){
+            if(freq<ent.getValue()){
+                freq = ent.getValue();
+                len = 0;
+            }
+            else if(freq==ent.getValue()){
+                len++;
+            }
+        }
+        int[] ar = new int[len];
+        len = 0;
+        for(Map.Entry<Integer, Integer> ent : map.entrySet()){
+            if(ent.getValue()==freq)
+                ar[len++] = ent.getKey();
+        }
+        return ar;
+    }
+    static void fun(TreeNode root){
+        if(map.containsKey(root.val))
+            map.put(root.val, map.get(root.val) + 1);
+        else
+            map.put(root.val, 1);
+        if(root.left!=null)
+            fun(root.left);
+        if(root.right!=null)
+            fun(root.right);
+    }
+}
