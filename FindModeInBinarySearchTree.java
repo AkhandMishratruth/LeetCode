23fec43bc52ae6cf283c4b49b1de5012c6e8a5b7
AkhandMishratruth <apratap.mishra.cer15@itbhu.ac.in>
2018-06-21 11:20:27 +0530

    501

--- FindModeInBinarySearchTree.java
+++ FindModeInBinarySearchTree.java
@@ -10,13 +10,13 @@
 class Solution {
     static HashMap<Integer, Integer> map;
     public int[] findMode(TreeNode root) {
-        map = new HashMap<Integer, Integer>();a
+        map = new HashMap<Integer, Integer>();
         fun(root);
         int len=0, freq=0;
         for(Map.Entry<Integer, Integer> ent : map.entrySet()){
             if(freq<ent.getValue()){
                 freq = ent.getValue();
-                len = 0;
+                len = 1;
             }
             else if(freq==ent.getValue()){
                 len++;
@@ -31,6 +31,8 @@ class Solution {
         return ar;
     }
     static void fun(TreeNode root){
+        if(root==null)
+            return;
         if(map.containsKey(root.val))
             map.put(root.val, map.get(root.val) + 1);
         else
